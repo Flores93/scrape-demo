bf7edba550dca7b8422c211b79d2c3c77cc64984
Jose Flores <luis.flores@raiven.com>
2022-01-31 14:06:46 -0600

    changes

--- index.js
+++ index.js
@@ -3,14 +3,10 @@ import puppeteer from "puppeteer";
 (async () => {
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
-  await page.goto("https://www.homedepot.com/p/313112375");
-  // await page.goto("https://www.homedepot.com/s/2554-20");
-  await page.waitForSelector(".fulfillment__header_zipcode");
-  // await page.waitForSelector(".store__success");
 
-  // const productAvailability = await page.$$eval("span.store__success", (ss) => {
-  //   return ss.map((av) => av.textContent);
-  // });
+  // product page
+  await page.goto("https://www.homedepot.com/p/313112375");
+  await page.waitForSelector(".fulfillment__header_zipcode");
 
   const zipCode = await page.$eval(
     "span.fulfillment__header_zipcode",
@@ -18,6 +14,15 @@ import puppeteer from "puppeteer";
   );
 
   console.log({ zipCode });
+
+  // results list
+  // await page.goto("https://www.homedepot.com/s/2554-20");
+  // await page.waitForSelector(".store__success");
+
+  // const productAvailability = await page.$$eval("span.store__success", (ss) => {
+  //   return ss.map((av) => av.textContent);
+  // });
+
   // console.log({ productAvailability });
 
   await browser.close();
